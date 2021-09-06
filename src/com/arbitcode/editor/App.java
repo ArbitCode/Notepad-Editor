2641fdefc1b63e7312d21886b0dddf684366c2a7
ArbitCode <rajaramsharma422@gmail.com>
2021-09-06 10:22:36 +0530

    changed logger

--- src/com/arbitcode/editor/App.java
+++ src/com/arbitcode/editor/App.java
@@ -4,6 +4,6 @@ public class App {
 	public static void main(final String[] args) {
 		System.out.println("App initiated");
 		final Notepad notePad = new Notepad();
-		System.out.println("App Closed");
+		System.out.println("App launched");
 	}
 }
